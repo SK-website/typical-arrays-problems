0ae6df2cdddc943245e75a9b6024843cb85a5b77
SK-website <vnali@tutanota.com>
2021-02-16 10:12:22 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+exports.min = function min(array) {
+    if (!array || array.length <= 0) return 0;
+    array.sort((prev, next) => prev - next);
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length <= 0) return 0;
+    array.sort((prev, next) => prev - next);
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return array[array.length - 1];
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length <= 0) return 0;
+    let res =
+        array.reduce((acc, el) => {
+            return acc + el;
+        }) / array.length;
+    return res;
+};
